32eb3797350eac466265aa88d843a199e4b5d063
Кирилл Киприянов <kinnder@mail.ru>
2016-11-04 14:39:14 +0300

    #6 State predicate added to PalleteOntology

--- src/palleteRobotCommunication/PalleteOntology.java
+++ src/palleteRobotCommunication/PalleteOntology.java
@@ -4,6 +4,7 @@ import jade.content.onto.BasicOntology;
 import jade.content.onto.Ontology;
 import jade.content.onto.OntologyException;
 import jade.content.schema.ConceptSchema;
+import jade.content.schema.PredicateSchema;
 import jade.content.schema.PrimitiveSchema;
 
 public class PalleteOntology extends Ontology {
@@ -15,6 +16,9 @@ public class PalleteOntology extends Ontology {
 	public static final String PALLETE = "Pallete";
 	public static final String PALLETE_MAX_CAPACITY = "maxCapacity";
 	public static final String PALLETE_CAPACITY = "capacity";
+	
+	public static final String STATE = "State";
+	public static final String STATE_DESCRIPTION = "description";
 
 	private static Ontology theInstance = new PalleteOntology();
 
@@ -30,6 +34,10 @@ public class PalleteOntology extends Ontology {
 			ConceptSchema cs = (ConceptSchema) getSchema(PALLETE);
 			cs.add(PALLETE_MAX_CAPACITY, (PrimitiveSchema) getSchema(BasicOntology.INTEGER));
 			cs.add(PALLETE_CAPACITY, (PrimitiveSchema) getSchema(BasicOntology.INTEGER));
+			
+			add(new PredicateSchema(STATE), State.class);
+			PredicateSchema ps = (PredicateSchema) getSchema(STATE);
+			ps.add(STATE_DESCRIPTION, (PrimitiveSchema) getSchema(BasicOntology.STRING));
 
 		} catch (OntologyException oe) {
 			oe.printStackTrace();
