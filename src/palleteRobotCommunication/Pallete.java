87616ddab12d8930a31ad856e047900f0fc65f6a
Кирилл Киприянов <kinnder@mail.ru>
2016-11-04 13:42:30 +0300

    #6 Pallete concept added

---
+++ src/palleteRobotCommunication/Pallete.java
@@ -0,0 +1,27 @@
+package palleteRobotCommunication;
+
+import jade.content.Concept;
+
+public class Pallete implements Concept {
+
+	private static final long serialVersionUID = 6518481924822268777L;
+
+	private int maxCapacity;
+	private int capacity;
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(int maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+}
