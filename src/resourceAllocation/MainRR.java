68afb3b46c32cfe7eec9f676bcaacbc99d8ac0ec
?????????»?» ?????????????????? <kinnder@mail.ru>
2017-05-11 15:47:08 +0300

    fixed package names

--- src/resourceAllocation/MainRR.java
+++ src/resourceAllocation/MainRR.java
@@ -6,11 +6,11 @@ public class MainRR {
     public static void main(String[] args) {
         String[] parameters = new String[2];
         parameters[0] = "-gui";
-        parameters[1] = "processor:com.resourceAllocation.processors.RoundRobinProcessorAgent;";
-        parameters[1] += "dummy1:com.resourceAllocation.DummyAgent(5);";
-        parameters[1] += "dummy2:com.resourceAllocation.DummyAgent(3);";
-        parameters[1] += "dummy3:com.resourceAllocation.DummyAgent(7);";
-        parameters[1] += "dummy4:com.resourceAllocation.DummyAgent(1);";
+        parameters[1] = "processor:resourceAllocation.processors.RoundRobinProcessorAgent;";
+        parameters[1] += "dummy1:resourceAllocation.DummyAgent(5);";
+        parameters[1] += "dummy2:resourceAllocation.DummyAgent(3);";
+        parameters[1] += "dummy3:resourceAllocation.DummyAgent(7);";
+        parameters[1] += "dummy4:resourceAllocation.DummyAgent(1);";
         Boot.main(parameters);
     }
 }
