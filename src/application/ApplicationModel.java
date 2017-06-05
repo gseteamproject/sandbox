fa1726e12240fc72adea62e1b20197f40fcea656
Кирилл Киприянов <kinnder@mail.ru>
2017-06-05 12:13:12 +0300

    special methods for adding agent into scenarios

--- src/application/ApplicationModel.java
+++ src/application/ApplicationModel.java
@@ -5,89 +5,97 @@ import bookTrading.BookTitle;
 public class ApplicationModel {
 	public String bookTrading() {
 		String scenario = new String();
-		scenario = "reader:bookTrading.BookBuyerAgent(" + BookTitle.LORD_OF_THE_RINGS + ");";
-		scenario += "noviceProgrammer:bookTrading.BookBuyerAgent(" + BookTitle.JAVA_TUTORIAL + ");";
-		scenario += "advancedProgrammer:bookTrading.BookBuyerAgent(" + BookTitle.JADE_PROGRAMMING_TUTORIAL + ");";
-		scenario += "seller1:bookTrading.BookSellerAgent;";
-		scenario += "seller2:bookTrading.BookSellerAgent;";
+		scenario = addAgent("reader", bookTrading.BookBuyerAgent.class, BookTitle.LORD_OF_THE_RINGS);
+		scenario += addAgent("noviceProgrammer", bookTrading.BookBuyerAgent.class, BookTitle.JAVA_TUTORIAL);
+		scenario += addAgent("advancedProgrammer", bookTrading.BookBuyerAgent.class, BookTitle.JADE_PROGRAMMING_TUTORIAL);
+		scenario += addAgent("seller1", bookTrading.BookSellerAgent.class);
+		scenario += addAgent("seller2", bookTrading.BookSellerAgent.class);
 		return scenario;
 	}
 
 	public String employment() {
 		String scenario = new String();
-		scenario = "engager:employment.EngagerAgent;";
-		scenario += "requester:employment.RequesterAgent;";
+		scenario = addAgent("engager", employment.EngagerAgent.class);
+		scenario += addAgent("requester", employment.RequesterAgent.class);
 		return scenario;
 	}
 
 	public String party() {
 		String scenario = new String();
-		scenario = "host:party.HostAgent;";
+		scenario = addAgent("host", party.HostAgent.class);
 		return scenario;
 	}
 
 	public String yellowPages() {
 		String scenario = new String();
-		scenario = "provider-1:yellowPages.DFRegisterAgent(my-forecast);";
-		scenario += "searcher:yellowPages.DFSearchAgent;";
-		scenario += "subscriber:yellowPages.DFSubscribeAgent;";
-		scenario += "provider-2:yellowPages.DFRegisterAgent(forecast-1);";
+		scenario = addAgent("provider-1", yellowPages.DFRegisterAgent.class, "my-forecast");
+		scenario += addAgent("searcher", yellowPages.DFSearchAgent.class);
+		scenario += addAgent("subscriber", yellowPages.DFSubscribeAgent.class);
+		scenario += addAgent("provider-2", yellowPages.DFRegisterAgent.class, "forecast-1");
 		return scenario;
 	}
 
 	public String contractNet() {
 		String scenario = new String();
-		scenario = "r1:protocols.ContractNetResponderAgent;";
-		scenario += "r2:protocols.ContractNetResponderAgent;";
-		scenario += "i:protocols.ContractNetInitiatorAgent(r1,r2);";
+		scenario = addAgent("r1", protocols.ContractNetResponderAgent.class);
+		scenario += addAgent("r2", protocols.ContractNetResponderAgent.class);
+		scenario += addAgent("i", protocols.ContractNetInitiatorAgent.class, "r1,r2");
 		return scenario;
 	}
 
 	public String broker() {
 		String scenario = new String();
-		scenario = "r:protocols.FIPARequestResponderAgent;";
-		scenario += "b:protocols.BrokerAgent(r);";
-		scenario += "i:protocols.FIPARequestInitiatorAgent(b)";
+		scenario = addAgent("r", protocols.FIPARequestResponderAgent.class);
+		scenario += addAgent("b", protocols.BrokerAgent.class, "r");
+		scenario += addAgent("i", protocols.FIPARequestInitiatorAgent.class, "b");
 		return scenario;
 	}
 
 	public String fipaRequest() {
 		String scenario = new String();
-		scenario = "r1:protocols.FIPARequestResponderAgent;";
-		scenario += "r2:protocols.FIPARequestResponderAgent;";
-		scenario += "i:protocols.FIPARequestInitiatorAgent(r1,r2);";
+		scenario = addAgent("r1", protocols.FIPARequestResponderAgent.class);
+		scenario += addAgent("r2", protocols.FIPARequestResponderAgent.class);
+		scenario += addAgent("i", protocols.FIPARequestInitiatorAgent.class, "r1,r2");
 		return scenario;
 	}
 
 	public String timeServer() {
 		String scenario = new String();
-		scenario = "server:ontology.ontologyServer.TimeServerAgent;";
-		scenario += "client:ontology.ontologyServer.TimeClientAgent;";
+		scenario = addAgent("server", timeServer.TimeServerAgent.class);
+		scenario += addAgent("client", timeServer.TimeClientAgent.class);
 		return scenario;
 	}
 
 	public String thanksAgent() {
 		String scenario = new String();
-		scenario = "thanksAgent:thanksAgent.ThanksAgent;";
+		scenario = addAgent("thanksAgent", thanksAgent.ThanksAgent.class);
 		return scenario;
 	}
 
 	public String palleteRobotCommunication() {
 		String scenario = new String();
-		scenario = "sourcePallete:palleteRobotCommunication.SourcePalleteAgent(3);";
-		scenario += "simpleRobot:palleteRobotCommunication.SimpleRobotAgent;";
+		scenario = addAgent("sourcePallete", palleteRobotCommunication.SimpleRobotAgent.class, "3");
+		scenario += addAgent("simpleRobot", palleteRobotCommunication.SimpleRobotAgent.class);
 		return scenario;
 	}
 
 	public String knowledge() {
 		String scenario = new String();
-		scenario = "kp:" + knowledge.KnowledgeProcessorAgent.class.getName() + ";";
+		scenario = addAgent("kp", knowledge.KnowledgeProcessorAgent.class);
 		return scenario;
 	}
 
 	public String transportline() {
 		String scenario = new String();
-		scenario = "line:transportsystem.TransportLineAgent;";
+		scenario = addAgent("line", transportsystem.TransportLineAgent.class);
 		return scenario;
+	}
+
+	private String addAgent(String agentName, Class<?> agentClass) {
+		return agentName + ":" + agentClass.getName() + ";";
+	}
+
+	private String addAgent(String agentName, Class<?> agentClass, String parameters) {
+		return agentName + ":" + agentClass.getName() + "(" + parameters + ");";
 	}
 }
