1eb4076bb4f59b368b30e0694b321987a3268fd0
kinnder <kinnder@mail.ru>
2017-06-06 14:04:51 +0300

    added empty KnowledgeProducerAgent class

---
+++ src/knowledge/producer/KnowledgeProducerAgent.java
@@ -0,0 +1,8 @@
+package knowledge.producer;
+
+import jade.core.Agent;
+
+public class KnowledgeProducerAgent extends Agent{
+	private static final long serialVersionUID = -8333173138628880437L;
+
+}
