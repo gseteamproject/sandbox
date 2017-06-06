e54a7dcb37629cfca9ca0cd03f3d95dcd27773c2
kinnder <kinnder@mail.ru>
2017-06-06 14:04:15 +0300

    added empty KnowledgeConsumerAgent class

---
+++ src/knowledge/consumer/KnowledgeConsumerAgent.java
@@ -0,0 +1,8 @@
+package knowledge.consumer;
+
+import jade.core.Agent;
+
+public class KnowledgeConsumerAgent extends Agent{
+	private static final long serialVersionUID = 1069538812627168203L;
+
+}
