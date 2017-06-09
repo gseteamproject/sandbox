16a9c0d260bceb72f37aebc725022b59ebd0e68b
kinnder <kinnder@mail.ru>
2017-06-09 14:47:29 +0400

    #9 conversationid for transportsystem-shuttle communication

--- src/transportsystem/TransportSystem.java
+++ src/transportsystem/TransportSystem.java
@@ -2,4 +2,6 @@ package transportsystem;
 
 public class TransportSystem {
 	final static public String SHUTTLE_ASKS_CAN_I_MOVE = "Можно ехать?";
+
+	final static public String TRANSPORTSYSTEM_SHUTTLE_ORDER = "shuttle-order";
 }
