22877b98993ba6cfedb4da775db435b8a26e95b5
The0081 <tpsshareduser@mail.ru>
2017-06-13 07:53:55 +0300

    #9 Проблема NotUnderstood решена

--- src/transportsystem/transportline/ListenBehaviour.java
+++ src/transportsystem/transportline/ListenBehaviour.java
@@ -4,32 +4,31 @@ import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import transportsystem.TransportSystem;
 
-
 public class ListenBehaviour extends CyclicBehaviour {
 
 	private static final long serialVersionUID = -4056102507089855423L;
-	
 
 	@Override
 	public void action() {
 		ACLMessage message = myAgent.receive();
-		
-		if (message != null){
+
+		if (message != null) {
 			TransportLineAgent myTransportLineAgent = (TransportLineAgent) myAgent;
 			myTransportLineAgent.trace(" Получен заказ: " + message.getContent());
 			String conversationId = message.getConversationId();
-			if(conversationId.equals(TransportSystem.TRANSPORTSYSTEM_TRANSPORTLINE_ORDER)){
-				myTransportLineAgent.registerOrder(message);				
-			} 
-			if(conversationId.equals(TransportSystem.TRANSPORTSYSTEM_SHUTTLE_ORDER)){
+			if (conversationId.equals(TransportSystem.TRANSPORTSYSTEM_TRANSPORTLINE_ORDER)) {
+				myTransportLineAgent.registerOrder(message);
+			}
+			if (conversationId.equals(TransportSystem.TRANSPORTSYSTEM_SHUTTLE_ORDER)) {
 				myTransportLineAgent.putBack(message);
 			}
-			else {
-				myTransportLineAgent.notUnderstood(message);
+			if (!conversationId.equalsIgnoreCase(TransportSystem.TRANSPORTSYSTEM_TRANSPORTLINE_ORDER)) {
+				if (!conversationId.equals(TransportSystem.TRANSPORTSYSTEM_SHUTTLE_ORDER)) {
+					myTransportLineAgent.notUnderstood(message);
+				}
 			}
-			
-		}
-		else{
+
+		} else {
 			block();
 		}
 	}
