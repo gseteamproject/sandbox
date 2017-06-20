13ba756eaf400bf5cc16fb31fbb9ea521102a74b
kinnder <kinnder@mail.ru>
2017-06-21 01:41:46 +0300

    #10 added tests for FindFactBehaviour

--- test/knowledge/consumer/FindFactBehaviourTest.java
+++ test/knowledge/consumer/FindFactBehaviourTest.java
@@ -8,13 +8,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import jade.content.ContentManager;
+import jade.content.abs.AbsObject;
 import jade.content.abs.AbsPredicate;
 import jade.content.lang.Codec.CodecException;
+import jade.content.lang.sl.SLVocabulary;
 import jade.content.onto.Ontology;
 import jade.content.onto.OntologyException;
 import jade.lang.acl.ACLMessage;
 import knowledge.KnowledgeAgent;
+import knowledge.ontology.Fact;
 import knowledge.ontology.KnowledgeOntology;
+import knowledge.ontology.Question;
 
 public class FindFactBehaviourTest {
 	private final Mockery context = new Mockery() {
@@ -35,7 +39,7 @@ public class FindFactBehaviourTest {
 
 		findFactBehaviour = new FindFactBehaviour(knowledgeAgent_mock, requestMessage_mock);
 	}
-	
+
 	@After
 	public void tearDown() {
 		context.assertIsSatisfied();
@@ -72,5 +76,102 @@ public class FindFactBehaviourTest {
 		});
 
 		findFactBehaviour.handleInform(informMessage_mock);
+	}
+
+	@Test
+	public void handleInform_answer() throws CodecException, OntologyException {
+		ACLMessage informMessage_mock = context.mock(ACLMessage.class, "inform message");
+		ContentManager contentManager_mock = context.mock(ContentManager.class);
+		AbsPredicate absPredicate_mock = context.mock(AbsPredicate.class);
+		Ontology ontology_mock = context.mock(Ontology.class);
+
+		Fact fact = new Fact("key", "value");
+		Question question = new Question();
+		question.setFact(fact);
+
+		context.checking(new Expectations() {
+			{
+				oneOf(knowledgeAgent_mock).trace("knowledge processor liked my question");
+
+				oneOf(knowledgeAgent_mock).getContentManager();
+				will(returnValue(contentManager_mock));
+
+				oneOf(contentManager_mock).extractAbsContent(informMessage_mock);
+				will(returnValue(absPredicate_mock));
+
+				oneOf(knowledgeAgent_mock).getContentManager();
+				will(returnValue(contentManager_mock));
+
+				oneOf(contentManager_mock).lookupOntology(KnowledgeOntology.ONTOLOGY_NAME);
+				will(returnValue(ontology_mock));
+
+				oneOf(absPredicate_mock).getTypeName();
+				will(returnValue(KnowledgeOntology.QUESTION));
+
+				oneOf(ontology_mock).toObject(absPredicate_mock);
+				will(returnValue(question));
+
+				oneOf(knowledgeAgent_mock).trace("answer question ( key [key] value [value] )");
+			}
+		});
+
+		findFactBehaviour.handleInform(informMessage_mock);
+	}
+
+	@Test
+	public void handleInform_no_answer() throws CodecException, OntologyException {
+		ACLMessage informMessage_mock = context.mock(ACLMessage.class, "inform message");
+		ContentManager contentManager_mock = context.mock(ContentManager.class);
+		AbsPredicate absPredicate_mock = context.mock(AbsPredicate.class);
+		Ontology ontology_mock = context.mock(Ontology.class);
+		AbsObject absObject_mock = context.mock(AbsObject.class);
+
+		Fact fact = new Fact("key", "value");
+		Question question = new Question();
+		question.setFact(fact);
+
+		context.checking(new Expectations() {
+			{
+				oneOf(knowledgeAgent_mock).trace("knowledge processor liked my question");
+
+				oneOf(knowledgeAgent_mock).getContentManager();
+				will(returnValue(contentManager_mock));
+
+				oneOf(contentManager_mock).extractAbsContent(informMessage_mock);
+				will(returnValue(absPredicate_mock));
+
+				oneOf(knowledgeAgent_mock).getContentManager();
+				will(returnValue(contentManager_mock));
+
+				oneOf(contentManager_mock).lookupOntology(KnowledgeOntology.ONTOLOGY_NAME);
+				will(returnValue(ontology_mock));
+
+				exactly(2).of(absPredicate_mock).getTypeName();
+				will(returnValue(SLVocabulary.NOT));
+
+				oneOf(absPredicate_mock).getAbsObject(SLVocabulary.NOT_WHAT);
+				will(returnValue(absObject_mock));
+
+				oneOf(ontology_mock).toObject(absObject_mock);
+				will(returnValue(question));
+
+				oneOf(knowledgeAgent_mock).trace("no answer for question ( key [key] )");
+			}
+		});
+
+		findFactBehaviour.handleInform(informMessage_mock);
+	}
+
+	@Test
+	public void handleRefuse() {
+		ACLMessage refuseMessage_mock = context.mock(ACLMessage.class, "refuse message");
+
+		context.checking(new Expectations() {
+			{
+				oneOf(knowledgeAgent_mock).trace("knowledge processor does not liked my question");
+			}
+		});
+
+		findFactBehaviour.handleRefuse(refuseMessage_mock);
 	}
 }
