efbcd128d60874dca0e711d56b47ec575436bb0f
Кирилл Киприянов <kinnder@mail.ru>
2017-06-22 15:03:36 +0300

    #12 removed redundant class WorkerContainer

--- src/resourceAllocation/core/Worker.java
+++ src/resourceAllocation/core/Worker.java
@@ -1,18 +1,21 @@
 package resourceAllocation.core;
 
+import java.util.Comparator;
+
 import jade.core.AID;
 
 public class Worker {
     private AID _agent;
     private long _time;
     private String _replyWith;
+    
+    public float waitingTime = 0;
 
     public Worker(AID agent, long time, String replyWith){
         _agent = agent;
         _time = time;
         _replyWith = replyWith;
-    }
-
+	}
 
     public AID getAgent(){
         return _agent;
@@ -25,4 +28,11 @@ public class Worker {
     public String getReplyWIth(){
         return _replyWith;
     }
+    
+    public final static Comparator<Worker> compareByTime = new Comparator<Worker>() {
+		@Override
+		public int compare(Worker o1, Worker o2) {
+			return (int) (o1.getTime() - o2.getTime());
+		}
+	};
 }
--- src/resourceAllocation/processors/ShortestJobFirstProcessorAgent.java
+++ src/resourceAllocation/processors/ShortestJobFirstProcessorAgent.java
@@ -2,98 +2,74 @@ package resourceAllocation.processors;
 
 import resourceAllocation.core.ProcessorAgent;
 import resourceAllocation.core.Worker;
-import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.OneShotBehaviour;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 
 public class ShortestJobFirstProcessorAgent extends ProcessorAgent {
 
 	private static final long serialVersionUID = -7482656135774231087L;
 
 	@Override
-    public void Serve(Worker[] agents) {
-        addBehaviour(new ServerBehaviour(this, agents));
-    }
+	public void Serve(Worker[] agents) {
+		addBehaviour(new ServerBehaviour(this, agents));
+	}
 
-    public void ShowStatistics(WorkerContainer[] workerContainers){
-        float loadTime = 0;
+	public void ShowStatistics(Worker[] workers) {
+		float loadTime = 0;
 
-        float leadTime = 0;
-        float leadTimeAverage = 0;
+		float leadTime = 0;
+		float leadTimeAverage = 0;
 
-        float waitingTime = 0;
-        float waitingTimeAverage = 0;
+		float waitingTime = 0;
+		float waitingTimeAverage = 0;
 
-        for (WorkerContainer workerContainer : workerContainers){
-            loadTime += workerContainer.Worker.getTime();
-            leadTime += workerContainer.Worker.getTime() + workerContainer.WaitingTime;
-            waitingTime += workerContainer.WaitingTime;
-        }
-
-        waitingTimeAverage = waitingTime / workerContainers.length;
-        leadTimeAverage = leadTime / workerContainers.length;
-
-        System.out.println("\nFull processing time is " + loadTime + " seconds.");
-        System.out.println("\nAverage waiting time is " + waitingTimeAverage + " seconds.");
-        System.out.println("\nAverage processing time is " + leadTimeAverage + " seconds.");
-    }
-
-    private class ServerBehaviour extends Behaviour {
-
-		private static final long serialVersionUID = -6262507239330173556L;
-		private boolean _isDone = false;
-        private ShortestJobFirstProcessorAgent _agent;
-        private ArrayList<WorkerContainer> _workerContainersList;
-
-        public ServerBehaviour(ShortestJobFirstProcessorAgent agent, Worker[] workers){
-            _agent = agent;
-            _workerContainersList = new ArrayList<WorkerContainer>();
-
-			for (Worker worker : workers) {
-				_workerContainersList.add(new WorkerContainer(worker));
-			}
-
-            _workerContainersList.sort(WorkerContainer.compareByWorkerTime);
-        }
-
-        @Override
-        public void action() {
-
-            for (int i = 0; i < _workerContainersList.size(); ++i) {
-                WorkerContainer currentWorkerContainer = _workerContainersList.get(i);
-                Worker currentWorker = currentWorkerContainer.Worker;
-
-                for (int j = i + 1; j < _workerContainersList.size(); ++j) {
-                    _workerContainersList.get(j).WaitingTime += currentWorker.getTime();
-                }
-
-                System.out.println("Agent " + currentWorker.getAgent().getName() + " was processed in " + currentWorker.getTime() + " seconds.");
-            }
-
-            _agent.ShowStatistics(_workerContainersList.toArray(new WorkerContainer[0]));
-            _isDone = true;
-        }
-
-        @Override
-        public boolean done() {
-            return _isDone;
-        }
-    }
-
-	private static class WorkerContainer {
-		public float WaitingTime = 0;
-		public Worker Worker;
-
-		public WorkerContainer(Worker worker) {
-			this.Worker = worker;
+		for (Worker worker : workers) {
+			loadTime += worker.getTime();
+			leadTime += worker.getTime() + worker.waitingTime;
+			waitingTime += worker.waitingTime;
 		}
 
-		public final static Comparator<WorkerContainer> compareByWorkerTime = new Comparator<WorkerContainer>() {
-			@Override
-			public int compare(WorkerContainer o1, WorkerContainer o2) {
-				return (int) (o1.Worker.getTime() - o2.Worker.getTime());
+		waitingTimeAverage = waitingTime / workers.length;
+		leadTimeAverage = leadTime / workers.length;
+
+		System.out.println("\nFull processing time is " + loadTime + " seconds.");
+		System.out.println("\nAverage waiting time is " + waitingTimeAverage + " seconds.");
+		System.out.println("\nAverage processing time is " + leadTimeAverage + " seconds.");
+	}
+
+	private class ServerBehaviour extends OneShotBehaviour {
+
+		private static final long serialVersionUID = -6262507239330173556L;
+		private ShortestJobFirstProcessorAgent _agent;
+		private ArrayList<Worker> _workerContainersList;
+
+		public ServerBehaviour(ShortestJobFirstProcessorAgent agent, Worker[] workers) {
+			_agent = agent;
+			_workerContainersList = new ArrayList<Worker>();
+
+			for (Worker worker : workers) {
+				_workerContainersList.add(worker);
 			}
-		};
+
+			_workerContainersList.sort(Worker.compareByTime);
+		}
+
+		@Override
+		public void action() {
+
+			for (int i = 0; i < _workerContainersList.size(); ++i) {
+				Worker currentWorker = _workerContainersList.get(i);
+
+				for (int j = i + 1; j < _workerContainersList.size(); ++j) {
+					_workerContainersList.get(j).waitingTime += currentWorker.getTime();
+				}
+
+				System.out.println("Agent " + currentWorker.getAgent().getName() + " was processed in "
+						+ currentWorker.getTime() + " seconds.");
+			}
+
+			_agent.ShowStatistics(_workerContainersList.toArray(new Worker[0]));
+		}
 	}
 }
