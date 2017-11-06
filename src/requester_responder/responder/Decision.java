9ca38abf8171b3947a797ed95399ef5bc9c396b4
kinnder <kinnder@mail.ru>
2017-11-06 21:41:06 +0300

    decision now has unpredictable behavior

--- src/requester_responder/responder/Decision.java
+++ src/requester_responder/responder/Decision.java
@@ -1,22 +1,32 @@
 package requester_responder.responder;
 
+import java.util.Random;
+
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 
 public class Decision extends OneShotBehaviour {
 
+	private final Random randomizer = new Random();
+
 	@Override
 	public void action() {
 		ActivityResponder parent = (ActivityResponder) getParent();
-		
 		ACLMessage request = (ACLMessage) getDataStore().get(parent.REQUEST_KEY);
-		
-		ACLMessage agree = request.createReply();
-		agree.setPerformative(ACLMessage.AGREE);
-		agree.setContent("now");
-		
-		getDataStore().put(parent.RESPONSE_KEY, agree);
+
+		int decision = randomizer.nextInt(100);
+
+		ACLMessage response = request.createReply();
+		if (decision > 50) {
+			response.setPerformative(ACLMessage.AGREE);
+			response.setContent("now");
+		} else {
+			response.setPerformative(ACLMessage.REFUSE);
+			response.setContent("busy doing another job");
+		}
+
+		getDataStore().put(parent.RESPONSE_KEY, response);
 	}
-	
+
 	private static final long serialVersionUID = 8554746504981566315L;
 }
