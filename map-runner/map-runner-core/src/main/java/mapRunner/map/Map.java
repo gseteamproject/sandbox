99a4d7359217f7a37d5b492466eed38cbb7ff157
kinnder <kinnder@mail.ru>
2018-04-06 13:15:21 +0300

    extended possible target to support forward, left, right - movement

--- map-runner/map-runner-core/src/main/java/mapRunner/map/Map.java
+++ map-runner/map-runner-core/src/main/java/mapRunner/map/Map.java
@@ -28,6 +28,12 @@ public class Map {
 			path.addPoint(Command.ROTATE_LEFT_90_DEGREE, 1);
 			path.addPoint(Command.FORWARD, 1);
 			path.addPoint(Command.ROTATE_180_DEGREE, 1);
+		} else if (target.id.equalsIgnoreCase("l")) {
+			path.addPoint(Command.ROTATE_LEFT_90_DEGREE, 1);
+		} else if (target.id.equalsIgnoreCase("r")) {
+			path.addPoint(Command.ROTATE_RIGHT_90_DEGREE, 1);
+		} else if (target.id.equalsIgnoreCase("f")) {
+			path.addPoint(Command.FORWARD, 1);
 		}
 		return path;
 	}
