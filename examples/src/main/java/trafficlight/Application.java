d1889d69deda69da3ddc5b8ed14169253e4dea7b
kinnder <kinnder@mail.ru>
2018-05-12 11:30:41 +0300

    application startup

--- examples/src/main/java/trafficlight/Application.java
+++ examples/src/main/java/trafficlight/Application.java
@@ -1,5 +1,6 @@
 package trafficlight;
 
+import application.ArgumentBuilder;
 import jade.Boot;
 
 public class Application {
@@ -7,7 +8,11 @@ public class Application {
 	public static void main(String[] args) {
 		String[] parameters = new String[2];
 		parameters[0] = "-gui";
-		parameters[1] = "tl-1:jade.core.Agent";
+		parameters[1] =
+			ArgumentBuilder.agent("traffic-light-1", TrafficLightAgent.class) +
+			ArgumentBuilder.agent("traffic-light-2", TrafficLightAgent.class) +
+			ArgumentBuilder.agent("car-traffic-1", CarTrafficLightAgent.class) + 
+			ArgumentBuilder.agent("car-traffic-2", CarTrafficLightAgent.class);
 		Boot.main(parameters);
 	}
 }
